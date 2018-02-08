303478532717035deb48935d66a25842ef004978
CodFrm <code.farmer@qq.com>
2018-02-08 14:13:25 +0800

    题目获取

--- src/cxmooc-tools/common.js
+++ src/cxmooc-tools/common.js
@@ -8,72 +8,106 @@ import {
 export function showExpand(_this) {
     var ans = _this.contentDocument.getElementsByClassName('ans-job-icon');
     for (var i = 0; i < ans.length; i++) {
-        if (ans[i].nextSibling.className.indexOf('ans-insertvideo-online') < 0) {
-            //判断是否为视频
-            continue;
-        }
-        console.log(_this.contentDocument.querySelector('iframe'));
-        console.log(_this.contentDocument.getElementsByTagName('iframe'));
-        var doc=_this.contentDocument.getElementsByTagName('iframe')[i].contentDocument;
-        console.log(doc);
         ans[i].style.width = '100%';
         ans[i].style.textAlign = 'center';
-        var hang = createBtn('开始挂机');
-        hang.value = i;
-        ans[i].appendChild(hang);
-        hang.onclick = function () {
-            var player = doc.querySelector('object');
-            var time = setInterval(function () {
-                try {
-                    player.playMovie();
-                } catch (e) {
-                    clearInterval(time);
-                }
-            }, 1000);
+        if (ans[i].nextSibling.className.indexOf('ans-insertvideo-online') >= 0) {
+            //视频
+            video(_this, ans[i], i);
+        } else {
+            //题目
+            topic(_this, ans[i], i);
         }
-        var boom = createBtn('秒过视频');
-        boom.value = i;
-        ans[i].appendChild(boom);
-        boom.onclick = function () {
-            //获取参数
-            var _index = this.value;
-            var mArg = _this.contentWindow.document.body.innerHTML;
-            mArg = '{' + substrEx(mArg, 'mArg = {', ';');
-            mArg = JSON.parse(mArg);
-            get('/ananas/status/' + mArg.attachments[_index].property.objectid +
-                '?k=318&_dc=' + Date.parse(new Date())).onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status != 200) {
-                        alert('未知错误');
-                    } else {
-                        //第二步
-                        var json = JSON.parse(this.responseText);
-                        var playTime = parseInt(json.duration - Math.random(1, 2));
-                        var enc = '[' + mArg.defaults.clazzId + '][' + mArg.defaults.userid + '][' +
-                            mArg.attachments[_index].property._jobid + '][' + mArg.attachments[_index].objectId + '][' +
-                            (playTime * 1000).toString() + '][d_yHJ!$pdA~5][' + (json.duration * 1000).toString() + '][0_' +
-                            json.duration + ']';
-                        enc = hex_md5(enc);
-                        get('/multimedia/log/' + json.dtoken + '?clipTime=0_' + json.duration +
-                            '&otherInfo=' + mArg.attachments[_index].otherInfo +
-                            '&userid=' + mArg.defaults.userid + '&rt=0.9&jobid=' + mArg.attachments[_index].property._jobid +
-                            '&duration=' + json.duration + '&dtype=Video&objectId=' + mArg.attachments[_index].objectId +
-                            '&clazzId=' + mArg.defaults.clazzId +
-                            '&view=pc&playingTime=' + playTime + '&isdrag=4&enc=' + enc).onreadystatechange = function () {
-                            if (this.readyState == 4) {
-                                if (this.status != 200) {
-                                    alert('未知错误');
-                                } else {
-                                    alert('成功刷新后查看效果');
-                                }
+    }
+}
+
+function topic(_this, elLogo, index) {
+    var doc = _this.contentDocument.getElementsByTagName('iframe')[index].contentDocument;
+    var topicDoc = doc.getElementById('frame_content').contentDocument;
+    var html = topicDoc.body.innerHTML;
+    var topicList = topicDoc.getElementsByClassName('Zy_TItle');
+    for (var i = 0; i < topicList.length; i++) {
+        console.log(getTopicMsg(topicList[i]));
+    }
+
+}
+
+/**
+ * 获取题目信息
+ * @param {*} elTopic 
+ */
+function getTopicMsg(elTopic) {
+    var msg = {};
+    msg.topic = elTopic.querySelector('div.clearfix').innerText;
+    msg.type = substrEx(msg.topic, '【', '】');
+    msg.topic = msg.topic.substring(msg.topic.indexOf('】') + 1);
+    msg.topic = msg.topic.substring(0, msg.topic.indexOf('（'));
+    var elOption = elTopic.nextSibling.nextSibling.getElementsByTagName('li');
+    msg.option = [];
+    for (var i = 0; i < elOption.length; i++) {
+        msg.option.push({
+            op: elOption[i].querySelector('.before input').value,
+            content: elOption[i].querySelector('.after').innerText,
+        });
+    }
+    return msg;
+}
+
+function video(_this, elLogo, index) {
+    var doc = _this.contentDocument.getElementsByTagName('iframe')[index].contentDocument;
+    var hang = createBtn('开始挂机');
+    hang.value = index;
+    elLogo.appendChild(hang);
+    hang.onclick = function () {
+        var player = doc.querySelector('object');
+        var time = setInterval(function () {
+            try {
+                player.playMovie();
+            } catch (e) {
+                clearInterval(time);
+            }
+        }, 1000);
+    }
+    var boom = createBtn('秒过视频');
+    boom.value = index;
+    elLogo.appendChild(boom);
+    boom.onclick = function () {
+        //获取参数
+        var _index = this.value;
+        var mArg = _this.contentDocument.body.innerHTML;
+        mArg = '{' + substrEx(mArg, 'mArg = {', ';');
+        mArg = JSON.parse(mArg);
+        get('/ananas/status/' + mArg.attachments[_index].property.objectid +
+            '?k=318&_dc=' + Date.parse(new Date())).onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status != 200) {
+                    alert('未知错误');
+                } else {
+                    //第二步
+                    var json = JSON.parse(this.responseText);
+                    var playTime = parseInt(json.duration - Math.random(1, 2));
+                    var enc = '[' + mArg.defaults.clazzId + '][' + mArg.defaults.userid + '][' +
+                        mArg.attachments[_index].property._jobid + '][' + mArg.attachments[_index].objectId + '][' +
+                        (playTime * 1000).toString() + '][d_yHJ!$pdA~5][' + (json.duration * 1000).toString() + '][0_' +
+                        json.duration + ']';
+                    enc = hex_md5(enc);
+                    get('/multimedia/log/' + json.dtoken + '?clipTime=0_' + json.duration +
+                        '&otherInfo=' + mArg.attachments[_index].otherInfo +
+                        '&userid=' + mArg.defaults.userid + '&rt=0.9&jobid=' + mArg.attachments[_index].property._jobid +
+                        '&duration=' + json.duration + '&dtype=Video&objectId=' + mArg.attachments[_index].objectId +
+                        '&clazzId=' + mArg.defaults.clazzId +
+                        '&view=pc&playingTime=' + playTime + '&isdrag=4&enc=' + enc).onreadystatechange = function () {
+                        if (this.readyState == 4) {
+                            if (this.status != 200) {
+                                alert('未知错误');
+                            } else {
+                                alert('成功刷新后查看效果');
                             }
                         }
                     }
                 }
-
             }
-        }
 
+        }
     }
 }
 /**
