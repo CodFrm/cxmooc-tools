2b9f44b70a5b1f5b463521be089aaa11a5da4729
CodFrm <code.farmer@qq.com>
2018-04-05 13:16:53 +0800

    v1.3.1

--- src/cxmooc-tools/popup.js
+++ src/cxmooc-tools/popup.js
@@ -1,6 +1,6 @@
 const moocConfig = require('../config');
 window.onload = function () {
-    document.getElementById('version').innerHTML='v'+moocConfig.version;
+    document.getElementById('version').innerHTML = 'v' + moocConfig.version.toString();
     var xhr = new XMLHttpRequest();
     xhr.open("GET", moocConfig.url + 'update', true);
     xhr.onreadystatechange = function () {
@@ -9,8 +9,8 @@ window.onload = function () {
                 var json = JSON.parse(this.responseText);
                 if (moocConfig.version < json.version) {
                     var p = document.createElement('p');
-                    p.style.color="#ff0000";
-                    p.innerHTML = '有新的版本更新:<a href="'+json.url+'" style="float:right;">点我去下载</a>  最新版本:v'+json.version;
+                    p.style.color = "#ff0000";
+                    p.innerHTML = '有新的版本更新:<a href="' + json.url + '" style="float:right;">点我去下载</a>  最新版本:v' + json.version;
                     document.getElementsByTagName('body')[0].appendChild(p);
                 }
             }
