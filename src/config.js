09afafbfbcb1253cd1ba635be3d37c5d02713984
CodFrm <code.farmer@qq.com>
2018-04-24 16:50:18 +0800

    Update v1.36 #11

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: "https://blog.icodef.com:8081/",
     version: 1.36,
-    update: 'https://blog.icodef.com/2018/01/25/1304',
-    enforce: true
+    update: 'https://github.com/CodFrm/cxmooc-tools/releases',
+    enforce: false
 }
