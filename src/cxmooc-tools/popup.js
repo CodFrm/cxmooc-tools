27fb1b6ebf8d246e8b3a9b02d52e77f3ae5de486
CodFrm <code.farmer@qq.com>
2018-04-24 16:55:40 +0800

    更新下载链接跳转bug修复

--- src/cxmooc-tools/popup.js
+++ src/cxmooc-tools/popup.js
@@ -10,7 +10,7 @@ window.onload = function () {
                 if (moocConfig.version < json.version) {
                     var p = document.createElement('p');
                     p.style.color = "#ff0000";
-                    p.innerHTML = '有新的版本更新:<a href="' + json.url + '" style="float:right;">点我去下载</a>  最新版本:v' + json.version;
+                    p.innerHTML = '有新的版本更新:<a href="' + json.url + '" style="float:right;" target="_blank">点我去下载</a>  最新版本:v' + json.version;
                     document.getElementsByTagName('body')[0].appendChild(p);
                 }
             }
