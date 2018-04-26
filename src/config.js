030310ad778d643bf53125865d8fc14f9889fff2
CodFrm <code.farmer@qq.com>
2018-04-26 10:40:12 +0800

    Merge branch 'master' of github.com:CodFrm/cxmooc-tools

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: "https://blog.icodef.com:8081/",
-    version: 1.32,
+    version: 1.36,
     update: 'https://github.com/CodFrm/cxmooc-tools/releases',
     enforce: false
 }
