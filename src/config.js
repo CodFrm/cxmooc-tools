07e69072d6f6a9fb067ba7193c28ce485d2f4bd7
CodFrm <code.farmer@qq.com>
2018-06-07 21:13:50 +0800

    v1.42

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: "https://blog.icodef.com:8081/",
-    version: 1.40,
+    version: 1.42,
     update: 'https://github.com/CodFrm/cxmooc-tools/releases',
     enforce: false
 }
