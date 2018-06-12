3faadcb8c1e5397e87c5932eb6f2d3943437024e
CodFrm <code.farmer@qq.com>
2018-06-12 10:27:47 +0800

    v1.46 自动挂机的更新 #23

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: "https://blog.icodef.com:8081/",
-    version: 1.42,
+    version: 1.46,
     update: 'https://github.com/CodFrm/cxmooc-tools/releases',
     enforce: false,
     cx: {
