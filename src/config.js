e747d1701971770732db09b9f1e42c923e4bfe9f
CodFrm <code.farmer@qq.com>
2018-06-22 10:06:44 +0800

    Update Readme.md & v1.48

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: "https://blog.icodef.com:8081/",
-    version: 1.46,
+    version: 1.48,
     update: 'https://github.com/CodFrm/cxmooc-tools/releases',
     enforce: false,
     cx: {
