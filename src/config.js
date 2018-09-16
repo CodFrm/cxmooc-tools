4ad5c5f3344509e98c7b7b91648cba0b622c5b4f
CodFrm <code.farmer@qq.com>
2018-09-16 08:50:04 +0800

    v1.51版本 修复超新h5播放器无法挂机的bug

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     url: "https://blog.icodef.com:8081/",
-    version: 1.5,
+    version: 1.51,
     update: 'https://github.com/CodFrm/cxmooc-tools/releases',
     enforce: false,
     cx: {
