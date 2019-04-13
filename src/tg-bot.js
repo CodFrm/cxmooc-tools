f907410d1327b58afd6a56d9133be645500e09d8
CodFrm <code.farmer@qq.com>
2019-04-14 00:50:58 +0800

    完善ci发布流程

--- src/tg-bot.js
+++ src/tg-bot.js
@@ -6,17 +6,19 @@ const botToken = process.env.BOT_TOKEN || '';
 const chat_id = process.env.GROUP_ID || '';
 const commit_range = process.env.TRAVIS_COMMIT_RANGE || '';
 const branch = process.env.TRAVIS_BRANCH || 'develop';
+const tag = process.env.TRAVIS_TAG || false;
+
 const tgBot = new TelegramBot(botToken, { polling: false });
 
-exec('git log --pretty=format:"%s" ' + commit_range, (err, stdout, stderr) => {
+exec('git log --pretty=format:"%s" ' + (branch == 'master' && tag ? tag + '..' : commit_range), (err, stdout, stderr) => {
     let sendText = '';
     let end = '';
-    if (branch == 'master') {
+    if (branch == 'master' && tag) {
         sendText += "*有一个新版本发布*\n";
         end = '\n[前去release查看](https://github.com/CodFrm/cxmooc-tools/releases)';
     } else {
         sendText += "*有一个内测版本发布*\n";
-        end='如果发现有什么BUG,记得[反馈](https://github.com/CodFrm/cxmooc-tools/issues)哦';
+        end = '如果发现有什么BUG,记得[反馈](https://github.com/CodFrm/cxmooc-tools/issues)哦';
     }
     sendText += "更新了以下内容:\n```" + stdout + "```" + end;
     tgBot.sendMessage(chat_id, sendText, { parse_mode: 'Markdown' });
