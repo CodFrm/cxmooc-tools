2516f4964618a754d58d55baa947404ba593379f
CodFrm <code.farmer@qq.com>
2019-04-19 23:54:07 +0800

    脚本优化

--- src/tampermonkey/zhihuishu-pack.js
+++ src/tampermonkey/zhihuishu-pack.js
@@ -1,8 +1,9 @@
 const zhihuishu = require('../cxmooc-tools/zhihuishu/zhihuishu');
 
 global.config = JSON.parse(localStorage['config']);
+config.duration = (config.interval || 0.1) * 60000;
 global.vtoken = config.vtoken;
 
 if (window.location.href.indexOf('zhihuishu.com/learning/videoList') > 0) {
-    zhihuishu.video.start();
+    zhihuishu.video();
 }
