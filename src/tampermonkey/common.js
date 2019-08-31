17e360ca7123fdb5aabb4bc1839f1435b1652a33
codfrm <code.farmer@qq.com>
2019-08-31 22:24:28 +0800

    修复油猴时间bug

--- src/tampermonkey/common.js
+++ src/tampermonkey/common.js
@@ -1,6 +1,7 @@
 const common = require('../cxmooc-tools/common')
 
 global.config = JSON.parse(localStorage['config']);
+config.duration = (config.interval || 0.1) * 60000;
 
 Object.defineProperty(global.config, 'duration', {
     get: function () {
