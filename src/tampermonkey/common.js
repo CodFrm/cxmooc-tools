5b4e0d6693c4cef12770905706c57d7da47f7e34
CodFrm <code.farmer@qq.com>
2019-09-25 14:24:12 +0800

    时间间隔缩短

--- src/tampermonkey/common.js
+++ src/tampermonkey/common.js
@@ -6,6 +6,6 @@ config.duration = (config.interval || 0.1) * 60000;
 Object.defineProperty(global.config, 'duration', {
     get: function () {
         let interval = (config.interval || 0.1) * 60000;
-        return common.randNumber(interval - interval / 2, interval + interval * 2);
+        return common.randNumber(interval - interval / 2, interval + interval / 2);
     }
 });
