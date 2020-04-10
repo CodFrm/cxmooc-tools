0d3c037a55cd8a0897ce6b27a045c172334b0ddb
[jhao0413] <[384716045@qq.com]>
2020-04-10 18:54:10 +0800

    布局更改

--- src/internal/utils/log.ts
+++ src/internal/utils/log.ts
@@ -1,6 +1,10 @@
-import {Application} from "../application";
+import {
+    Application
+} from "../application";
 import "../../views/common";
-import {Noifications} from "@App/internal/utils/utils";
+import {
+    Noifications
+} from "@App/internal/utils/utils";
 
 export interface Logger {
     Debug(...args: any): Logger;
@@ -56,15 +60,42 @@ export class PageLog implements Logger {
         return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
     }
 
+    first(text: string, color: string, background: string) {
+        let new_log = document.createElement("div");
+        new_log.innerHTML = `
+                <div class="log" style="border-color: ` + background + `; background-color: ` + background + `;">
+                    <p><span style="color:` + color + `;">` + text + `</span></p>
+                </div>
+            `;
+        //插入第一个元素前
+        var first = document.getElementsByClassName("tools-notice-content")[0].getElementsByTagName("div");
+        document.querySelector(".tools-notice-content").insertBefore(new_log, first[0]);
+    }
+
     constructor() {
         this.el = undefined;
         window.addEventListener("load", () => {
             let div = document.createElement("div");
-            div.innerHTML = '小工具通知条<button class="close">关闭</button><div class="tools-notice-content">';
+            // 主要布局
+            div.innerHTML = `
+            <div class="head"> 
+               <span>小工具通知条</span> 
+               <label class="switch" style="width:90px">
+                  <input class="checkbox-input" id="checkbox" type="checkbox" checked="checked">
+                  <label class="checkbox" for="checkbox"></label>
+                  <span>桌面通知</span>
+               </label>
+               <span class="close" style="float:right; cursor:pointer; margin-right:5px;">x</span>
+            </div>
+            <div class="main">
+               <div class="tools-notice-content"></div>
+            </div>
+            `;
+
             div.className = "tools-logger-panel";
             document.body.appendChild(div);
             this.el = div.querySelector(".tools-notice-content");
-            (<HTMLButtonElement>div.querySelector("button.close")).onclick = () => {
+            ( < HTMLButtonElement > div.querySelector(".close")).onclick = () => {
                 this.el = undefined;
                 div.remove();
             };
@@ -90,8 +121,10 @@ export class PageLog implements Logger {
 
     public Info(...args: any): Logger {
         let text = this.toStr(...args);
-        if (this.el) {
-            this.el.innerHTML = text
+        // 判断选中状态是否发送桌面通知
+        if (this.el && ( < HTMLInputElement > this.el.querySelector('#checkbox')).checked) {
+            var s1 = new PageLog();
+            s1.first(text, "#409EFF", "rgba(121, 187, 255, 0.2)");
         } else {
             console.info("[info", this.getNowTime(), "]", ...args);
         }
@@ -103,9 +136,10 @@ export class PageLog implements Logger {
         console.warn("[warn", this.getNowTime(), "]", ...args);
         let text = this.toStr(...args);
         if (this.el) {
-            this.el.innerHTML = text;
+            var s1 = new PageLog();
+            s1.first(text, "#5C3C00", "rgba(250, 236, 216, 0.4)");
         }
-        if (document.hidden) {
+        if (document.hidden && ( < HTMLInputElement > this.el.querySelector('#checkbox')).checked) {
             Noifications({
                 title: "超星慕课小工具",
                 text: text,
@@ -118,13 +152,16 @@ export class PageLog implements Logger {
     public Error(...args: any): Logger {
         console.error("[error", this.getNowTime(), "]", ...args);
         let text = this.toStr(...args);
-        if (this.el) {
-            this.el.innerHTML = text;
+        if (this.el && ( < HTMLInputElement > this.el.querySelector('#checkbox')).checked) {
+            var s1 = new PageLog();
+            s1.first(text, "#FFF0F0", "rgba(253, 226, 226, 0.5)");
         }
-        Noifications({
-            title: "超星慕课小工具",
-            text: text,
-        });
+        if (document.hidden && ( < HTMLInputElement > this.el.querySelector('#checkbox')).checked) {
+            Noifications({
+                title: "超星慕课小工具",
+                text: text,
+            });
+        }
         return this;
     }
 
@@ -132,10 +169,12 @@ export class PageLog implements Logger {
         console.error("[fatal", this.getNowTime(), "]", ...args);
         let text = this.toStr(...args);
         this.el.innerHTML = text;
-        Noifications({
-            title: "超星慕课小工具",
-            text: text,
-        });
+        if (document.hidden && ( < HTMLInputElement > this.el.querySelector('#checkbox')).checked) {
+            Noifications({
+                title: "超星慕课小工具",
+                text: text,
+            });
+        }
         return this;
     }
 }
