c65b6fc045b79c8c1ae2c951ae822e22950ee0af
CodFrm <code.farmer@qq.com>
2020-09-03 16:05:40 +0800

    各平台默认做题时间

--- src/mooc/course163/platform.ts
+++ src/mooc/course163/platform.ts
@@ -1,10 +1,10 @@
-import { MoocFactory, Mooc } from "../factory";
-import { Course163 } from "./course163";
+import {MoocFactory, Mooc} from "../factory";
+import {Course163} from "./course163";
 import {Application} from "@App/internal/application";
 
 export class Course163Platform implements MoocFactory {
     public CreateMooc(): Mooc {
-        Application.App.config.topic_interval = 0.05;
+        Application.App.config.topic_interval = Application.App.config.topic_interval || 0.05;
         return new Course163();
     }
 }
--- src/mooc/zhihuishu/platform.ts
+++ src/mooc/zhihuishu/platform.ts
@@ -5,7 +5,7 @@ import {Application} from "@App/internal/application";
 
 export class ZhsPlatform implements MoocFactory {
     public CreateMooc(): Mooc {
-        Application.App.config.topic_interval = 0;
+        Application.App.config.topic_interval = Application.App.config.topic_interval || 0;
         let mooc: Mooc = null;
         if (document.URL.indexOf("studyh5.zhihuishu.com/videoStudy.html") > 0) {
             mooc = new ZhsVideo();
